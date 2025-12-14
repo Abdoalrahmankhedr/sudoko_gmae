cd2b5eb456235f6cd04e83f639003c1a1ab85a07
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2025-12-14 16:39:54 +0200

    add comments

--- src/main/java/controller/Viewable.java
+++ src/main/java/controller/Viewable.java
@@ -10,10 +10,40 @@ import model.Game;
 import java.io.IOException;
 
 public interface Viewable {
+    /*
+        There should be a GameCatalogue class which would return
+        a Catalog model with the correct values.
+    */
     Catalog getCatalog();
+
+    /*
+        Use the GameManager here to simply load the game by difficulty enum
+    */
     Game getGame(Difficulty level) throws GameNotFoundException;
+
+    /*
+        Use the GameGenerator to generate the three difficulties, use the GameManager in composition
+        to save the boards to file.
+    */
     void driveGames(Game source) throws SolutionInvalidException;
+
+    /*
+        Use the Verifier to obtain the duplicate locations and then format it
+    */
     String verifyGame(Game game);
+
+    /*
+        This should use the game Solver to solve the board, then only return the
+        an array of the index followed by the answer to the missing cell in that index
+        [1,2,10,8,12,4] -> Cell 1 has 2, Cell 10 has 8, Cell 12 has 4
+                        -> {0,0} has 2, {1,0} has 8, {1,2} has 4
+    */
     int[] solveGame(Game game) throws GameInvalidException;
+
+    /*
+        Simply use new UserAction(userAction) to get an entry.
+        Then call:
+            UserActionLogger.record(new UserAction(userAction));
+    */
     void logUserAction(String userAction) throws IOException;
 }
--- src/main/java/views/Controllable.java
+++ src/main/java/views/Controllable.java
@@ -8,11 +8,48 @@ import model.UserAction;
 
 import java.io.IOException;
 
+/*
+    A class implementing this interface would have a Viewable dependency
+    Therefore, these methods will adapt the parameters to fit with the parameters of the dependency
+
+    Andrew :)
+*/
 public interface Controllable {
+    /*
+        There should be a GameCatalogue class which would return
+        a Catalog model with the correct values.
+    */
     Catalog getCatalog();
+
+    /*
+        use Difficulty.fromChar(level) to use in Viewable.getGame()
+        then simply return the board in the Game model
+    */
     int[][] getGame(char level) throws GameNotFoundException;
-    void driveGames(int[][] source) throws SolutionInvalidException;
+
+    /*
+        Can use BoardManager.getFromFile() to get int[][] of board, convert that to Game model
+        then use Viewable.driveGames()
+    */
+    void driveGames(String sourcePath) throws SolutionInvalidException;
+
+    /*
+        Convert the board to a Game model, then send to Viewable.verifyGame()
+        you will receive a string where if valid -> bool[][] is all true
+                                        if invalid -> extract duplicate locations, then add false
+                                                      in these indexes on the board
+    */
     boolean[][] verifyGame(int[][] game);
+
+    /*
+        Convert the board to a Game model, then send to Viewable.solveGame()
+        you will receive the index followed by value as in example in Viewable.solveGame(),
+            then map it to the same board and return that.
+    */
     int[][] solveGame(int[][] game) throws GameInvalidException;
+
+    /*
+        Use Viewable.logUserAction()
+    */
     void logUserAction(UserAction userAction) throws IOException;
 }
