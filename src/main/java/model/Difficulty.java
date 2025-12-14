e543a44345dbbb8330dc2f7d2cbebbec4a7abfec
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2025-12-14 16:39:41 +0200

    update difficulty enum

--- src/main/java/model/Difficulty.java
+++ src/main/java/model/Difficulty.java
@@ -1,5 +1,30 @@
 package model;
 
 public enum Difficulty {
-    EASY, MEDIUM, HARD
+    EASY('E'),
+    MEDIUM('M'),
+    HARD('H');
+
+    private final char code;
+
+    Difficulty(char code) {
+        this.code = code;
+    }
+
+    /* Get enum from code */
+    public Difficulty fromChar(char c) {
+        char upperCaseCode = Character.toUpperCase(code);
+
+        // Iterate through all constants in the enum
+        for (Difficulty difficulty : values()) {
+            if (difficulty.getCode() == upperCaseCode) {
+                return difficulty;
+            }
+        }
+
+        throw new IllegalArgumentException("[Difficulty] invalid enum code: " + code);
+    }
+
+    /* Getters */
+    public char getCode() {return this.code;}
 }
