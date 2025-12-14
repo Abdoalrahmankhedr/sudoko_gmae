e31f9677aaa2bed715f573034f0600f88f1fdf03
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2025-12-14 16:21:12 +0200

    update logging classes

--- src/main/java/service/log/UserActionLogger.java
+++ src/main/java/service/log/UserActionLogger.java
@@ -6,21 +6,42 @@ import model.UserAction;
 /*
     Uses the idea of an Adapter to connect between String & UserAction
 */
-public class UserActionLogger {
+public class UserActionLogger implements Logger<UserAction> {
+    /* Default location to the log file, there is only one */
+    private static final String logPath = "src/main/java/resources/incomplete/log.txt";
+    /* Default location to the log file, there is only one */
+    private final FileLogger logger = new FileLogger(logPath);
+    /* Apply singleton pattern */
+    private static UserActionLogger instance;
+
+    private UserActionLogger() {}
+
+    /* Apply singleton pattern */
+    /* Make sure only one action logger is ever made in its lifecycle */
+    public static UserActionLogger getInstance() {
+        if (instance == null) {
+            instance = new UserActionLogger();
+        }
+        return instance;
+    }
+
     /* Add a new log entry */
-    public static void record(UserAction action) {
-        FileLogger.record(action.toString() + "\n");
+    @Override
+    public void record(UserAction action) {
+        logger.record(action.toString() + "\n");
     }
 
     /* Remove the latest log entry */
-    public static UserAction removeLast() {
-        String last = FileLogger.removeLast();
+    @Override
+    public UserAction removeLast() {
+        String last = logger.removeLast();
         if (last != null) return new UserAction(last);
         else return null;
     }
 
     /* Delete log file */
-    public static void delete() {
-        FileLogger.delete();
+    @Override
+    public void delete() {
+        logger.delete();
     }
 }
